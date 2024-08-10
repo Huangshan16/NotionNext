e2b4ec70dd74a70519d89864e0e99824069fdeec
Huangshan16 <89714227+Huangshan16@users.noreply.github.com>
2024-08-10 14:35:23 +0800

    Update Footer.js
    hide the copyright msg

--- themes/starter/components/Footer.js
+++ themes/starter/components/Footer.js
@@ -143,7 +143,8 @@ export const Footer = props => {
                   </div>
                 </div>
               </div>
-              <div className='w-full px-4 md:w-1/3 lg:w-1/2'>
+              {/* copyright */}
+              {/* <div className='w-full px-4 md:w-1/3 lg:w-1/2'>
                 <div className='my-1 flex justify-center md:justify-end'>
                   <p className='text-base text-gray-7'>
                     Designed and Developed by
@@ -156,7 +157,8 @@ export const Footer = props => {
                     </a>
                   </p>
                 </div>
-              </div>
+              </div> */}
+
             </div>
           </div>
         </div>
