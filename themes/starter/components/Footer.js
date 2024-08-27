825f271b2c77e9e6b96a190a20ff1e132be2a461
Huangshan16 <1550465685@qq.com>
2024-08-28 00:06:02 +0800

    Footer update for copyright of MIT

--- themes/starter/components/Footer.js
+++ themes/starter/components/Footer.js
@@ -144,7 +144,7 @@ export const Footer = props => {
                 </div>
               </div>
               {/* copyright */}
-              {/* <div className='w-full px-4 md:w-1/3 lg:w-1/2'>
+              <div className='w-full px-4 md:w-1/3 lg:w-1/2'>
                 <div className='my-1 flex justify-center md:justify-end'>
                   <p className='text-base text-gray-7'>
                     Designed and Developed by
@@ -157,7 +157,7 @@ export const Footer = props => {
                     </a>
                   </p>
                 </div>
-              </div> */}
+              </div>
 
             </div>
           </div>
