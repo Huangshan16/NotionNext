69d3981c25daadb1bc658afb53415d4cc579c36a
Huangshan16 <1550465685@qq.com>
2024-09-22 16:20:09 +0800

    update ICP certification log4

--- themes/starter/components/Footer.js
+++ themes/starter/components/Footer.js
@@ -155,15 +155,15 @@ export const Footer = props => {
                       className='px-1 text-gray-1 hover:underline'>
                       NotionNext {siteConfig('VERSION')}
                     </a> */}
-                    <img 
-                      src='images/starter/备案图标.png' 
-                      alt='公安备案' 
-                      className='mt-2 mr-2' 
-                    />
                     <a 
                       href="https://beian.mps.gov.cn/#/query/webSearch?code=11010502055819" 
                       rel="noreferrer" 
                       target="_blank">
+                      <img 
+                        src='images/starter/备案图标.png' 
+                        alt='公安备案' 
+                        className='mt-2 mr-2' 
+                      />
                       京公网安备11010502055819
                     </a>
                   </p>
